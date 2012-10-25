6d68f707e46883671146076b4350e12366cb3e2f
Lucas Videla <videla.lucas@gmail.com>
2012-10-25 15:33:12 -0300

    se mejora el test de integral de area

--- src/test/java/ar/edu/unlam/tallerweb/ecuaciones/IntegralTest.java
+++ src/test/java/ar/edu/unlam/tallerweb/ecuaciones/IntegralTest.java
@@ -14,14 +14,18 @@ public class IntegralTest {
 	@Test
 	public void areaBajoLaIdentidad() {
 
-		Ecuacion ecuacion = new EcuacionLineal(0.0, 1.0);
+		Ecuacion ecuacion = new EcuacionLineal(1.0, 0.0);
 
-		IntegralDeArea i = new IntegralDeArea();
-		Double area = i.calcular(ecuacion, 0.0, 1.0, 0.01);
+		IntegralDeArea integral = new IntegralDeArea();
+		Double incremento = 0.01;
+		Double areaObtenida = integral.calcular(ecuacion, 0.0, 1.0, incremento);
 
+		
+		Double precision = 0.01;
+		Double areaEsperada = 0.5;
 		assertEquals(
 				"Se espera que el área bajo la recta identidad entre 0 y 1 sea 1/2",
-				0.5, area.doubleValue(), 0.01);
+				areaEsperada, areaObtenida.doubleValue(), precision);
 
 	}
 
